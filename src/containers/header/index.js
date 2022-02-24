531fb29c538162301cbe7a773594906519ef5f7e
eminkarabudak <eminkarabudak@gmail.com>
2022-02-25 02:56:10 +0300

    added header and brand components

--- src/containers/header/index.js
+++ src/containers/header/index.js
@@ -1,8 +1,32 @@
+import Ai from '../../assets/images/ai.png'
+import people from '../../assets/images/people.png'
 import './header.scss';
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='header section__padding'>
+      <div className="header__content">
+        <div className="header__content--text">
+          <h1 className='gradient__text'>Let’s Build Something
+            amazing with GPT-3
+          </h1>
+          <p>
+            Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.
+          </p>
+        </div>
+        <div className="header__content--input">
+          <input type="email" placeholder='Your Email Address' />
+          <button type='button' >Get Started</button>
+        </div>
+        <div className="header__content--people">
+          <img src={people} alt="people" />
+          <p>1,600 people requested access a visit in last 24 hours</p>
+        </div>
+      </div>
+      <div className="header__img">
+        <img src={Ai} alt="Ai" />
+      </div>
+    </div>
   )
 }
 
