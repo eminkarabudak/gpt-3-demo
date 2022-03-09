d60b6763ba8fb14200673a9b925729fbfadf82a9
TURKCELL\EXT02D25964 <emin.karabudag@consultant.turkcell.com.tr>
2022-03-09 13:24:19 +0300

    fix

--- src/components/brand/index.js
+++ src/components/brand/index.js
@@ -1,23 +1,34 @@
-import './brand.scss'
-import { google, slack, dropbox, shopify, atlassian } from "./imports"
+import './brand.scss';
+
 const Brand = () => {
+  const brandData = [{
+    title: "google",
+    src: `${require("../../assets/images/google.png")}`
+  },
+  {
+    title: "slack",
+    src: `${require("../../assets/images/slack.png")}`
+  },
+  {
+    title: "dropbox",
+    src: `${require("../../assets/images/dropbox.png")}`
+  },
+  {
+    title: "shopify",
+    src: `${require("../../assets/images/shopify.png")}`
+  },
+  {
+    title: "atlassian",
+    src: `${require("../../assets/images/atlassian.png")}`
+  }]
+
   return (
     <div className='brand section__padding'>
-      <div>
-        <img src={google} alt="google" />
-      </div>
-      <div>
-        <img src={slack} alt="slack" />
-      </div>
-      <div>
-        <img src={dropbox} alt="dropbox" />
-      </div>
-      <div>
-        <img src={shopify} alt="shopify" />
-      </div>
-      <div>
-        <img src={atlassian} alt="atlassian" />
-      </div>
+      {brandData.map((item, index) => (
+        <div key={index}>
+          <img src={item.src} alt={item.title} />
+        </div>
+      ))}
     </div>
   )
 }
